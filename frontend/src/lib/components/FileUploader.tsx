86ad98fa2d337f0d6e77343254dcb206708e2188
Nicole Venner <nvenner@protonmail.ch>
2024-06-25 15:15:46 -0600

    typo

--- frontend/src/lib/components/FileUploader.tsx
+++ frontend/src/lib/components/FileUploader.tsx
@@ -47,7 +47,7 @@ const UploadFileButton: React.FC = () => {
     formData.append("file", file);
 
     try {
-      await axios.post("/api/upload", formData, {
+      await axios.post("/api/file/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
